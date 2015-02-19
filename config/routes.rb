c7faaf0c0c0c7f618080143d1d7d2f093d5f7d2d
Joel Brewer <joelbrewer01@gmail.com>
2015-02-18 19:27:45 -0600

    add update action to the users controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json },
                               constraints: { subdomain: 'api' }, path: '/' do
     scope module: :v1 do
+      resources :users, :only => [:show, :create, :update]
     end
   end
 end
