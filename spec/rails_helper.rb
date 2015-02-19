75b645a353b8ca4321a02b9dcd06412c6a83ea91
Joel Brewer <joelbrewer01@gmail.com>
2015-02-18 19:59:36 -0600

    Add sessions controller create action.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -51,6 +51,7 @@ RSpec.configure do |config|
   # Including test requests
   config.include Request::JsonHelpers, :type => :controller
   config.include Request::HeadersHelpers, :type => :controller
+  config.include Devise::TestHelpers, :type => :controller
   
   config.before(:each, type: :controller) do
     api_response_format
