78fb4ca63f68804b76ae430d1aa9ed28b18d070b
Joel Brewer <joelbrewer01@gmail.com>
2015-02-18 19:31:14 -0600

    Add destroy action to users controller.

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json },
                               constraints: { subdomain: 'api' }, path: '/' do
     scope module: :v1 do
-      resources :users, :only => [:show, :create, :update]
+      resources :users, :only => [:show, :create, :update, :destroy]
     end
   end
 end
